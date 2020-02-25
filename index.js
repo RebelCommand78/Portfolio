9bb78edc63a7c695ac73e32db5586363e03bdb6b
stovallmi <martinstovall@yahoo.com>
2020-02-24 21:03:18 -0700

    navbar fixes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ $(function() {
     isMobile = true;
 
     // Mobile height fix
-    $('.height-fix').each(function() {
+    $(".height-fix").each(function() {
       var h = $(this).height();
       $(this).height(h);
     });
@@ -16,91 +16,69 @@ $(function() {
 
   // Sticky Nav on Mobile
   if (isMobile) {
-    $('nav').addClass('fixed');
+    $("nav").addClass("fixed");
   } else {
-    $('nav').addClass('desk');
+    $("nav").addClass("desk");
   }
 
   // NAV POSITION
-  var navPos = $('nav').position().top;
+  var navPos = $("nav").position().top;
   var lastPos = 0;
   var lockTimer;
 
-  $(window).on('scroll', function() {
+  $(window).on("scroll", function() {
     var pos = $(window).scrollTop();
     var pos2 = pos + 50;
     var scrollBottom = pos + $(window).height();
 
     if (!isMobile) {
-      if (pos >= navPos + $('nav').height() && lastPos < pos) {
-        $('nav').addClass('fixed');
+      if (pos >= navPos + $("nav").height() && lastPos < pos) {
+        $("nav").addClass("fixed");
       }
       if (pos < navPos && lastPos > pos) {
-        $('nav').removeClass('fixed');
+        $("nav").removeClass("fixed");
       }
       lastPos = pos;
     }
 
     // Link Highlighting
-    if (pos2 > $('#home').offset().top) {
-      highlightLink('Home');
+    if (pos2 > $("#home").offset().top) {
+      highlightLink("Home");
     }
-    if (pos2 > $('#about').offset().top) {
-      highlightLink('About');
+    if (pos2 > $("#about").offset().top) {
+      highlightLink("About");
     }
-    if (pos2 > $('#portfolio').offset().top) {
-      highlightLink('Portfolio');
+    if (pos2 > $("#portfolio").offset().top) {
+      highlightLink("Portfolio");
     }
     if (
-      pos2 > $('#contact').offset().top ||
+      pos2 > $("#contact").offset().top ||
       pos + $(window).height() === $(document).height()
     ) {
-      highlightLink('Contact');
+      highlightLink("Contact");
     }
 
     // Prevent Hover on Scroll
     clearTimeout(lockTimer);
-    if (!$('body').hasClass('disable-hover')) {
-      $('body').addClass('disable-hover');
+    if (!$("body").hasClass("disable-hover")) {
+      $("body").addClass("disable-hover");
     }
 
     lockTimer = setTimeout(function() {
-      $('body').removeClass('disable-hover');
+      $("body").removeClass("disable-hover");
     }, 500);
   });
 
   function highlightLink(anchor) {
-    $('nav .active').removeClass('active');
-    $('nav')
+    $("nav .active").removeClass("active");
+    $("nav")
       .find('[href="' + anchor + '"]')
-      .addClass('active');
+      .addClass("active");
   }
 
   // EVENT HANDLERS
-  $('.nav-link').click(function() {
-    var anchor = $(this).attr('href');
-    $('.link-wrap').removeClass('visible');
 
-    $('nav span').removeClass('active');
-    $('nav')
-      .find('[href="' + anchor + '"]')
-      .addClass('active');
-
-    $('html, body').animate(
-      {
-        scrollTop: $('#' + anchor).offset().top
-      },
-      400
-    );
-  });
-
-  $('.mdi-menu').click(function() {
-    $('.link-wrap').toggleClass('visible');
-  });
 
 
   // CONTACT FORM
-  
-
-  
 });
